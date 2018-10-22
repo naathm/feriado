06d026fc6ed58ba78a6cc0ab30aa1e5831fd4eff
Marcus Lima <marcus_fonseca@ymail.com>
2018-10-21 21:04:13 -0300

    Corrigido problema com pasta onde se encontra o clone do repositorio

--- session.php
+++ session.php
@@ -3,13 +3,22 @@ session_start();
 
 $host = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 
-$login_page = 'localhost/feriado/login.php';
-$login_page_error1 = 'localhost/feriado/login.php?erro=154';
-$login_page_error2 = 'localhost/feriado/login.php?erro=171';
-$cadastro_page = 'localhost/feriado/cadastro.php';
+$server_name = $_SERVER['SERVER_NAME'];
+$dirname = dirname(__FILE__);
+$folder_name = substr_replace($dirname,'',0,14);
 
-$exception_array = array($login_page,$cadastro_page);
-$login_page_errors = array($login_page_error1,$login_page_error2);
+$login_page = 'login.php';
+$login_page_error_154 = 'login.php?erro=154';
+$login_page_error_171 = 'login.php?erro=171';
+$cadastro_page = 'cadastro.php';
+
+$login_page_path = $server_name . "/" . $folder_name . "/" . $login_page;
+$login_page_error_154_path = $server_name . "/" . $folder_name . "/" . $login_page_error_154;
+$login_page_error_171_path = $server_name . "/" . $folder_name . "/" . $login_page_error_171;
+$cadastro_page_path = $server_name . "/" . $folder_name . "/" . $cadastro_page;
+
+$exception_array = array($login_page_path,$cadastro_page_path);
+$login_page_errors = array($login_page_error_154_path,$login_page_error_171_path);
 $array_merged = array_merge($exception_array, $login_page_errors);
 
 
