ffe71241b95cae68506654695420d67c5285d9fa
Wendel Sodre <wendelsodres@gmail.com>
2018-10-26 00:54:11 -0300

    Removendo padding do body

--- header.php
+++ header.php
@@ -13,7 +13,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <style>
         body {
-            padding-top: 3.5em;
+            margin:0;
+            padding:0;
         }
     </style>
 </head>
